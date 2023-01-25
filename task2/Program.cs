a6ef32195a19e09ee80aedfec99e806d621ca26b
Aleksey <lexandreeev@gmail.com>
2023-01-25 20:27:45 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Подсчитать сумму цифр в числе");
+Console.WriteLine("Введите любое число:");
+int number = int.Parse(Console.ReadLine()!);
+string numberStr = Convert.ToString(number);
+int sum=0;
+for(int i = 0;i<numberStr.Length;i++){
+int digit = number%10;
+sum = sum + digit;
+number=number/10;
+}
+Console.WriteLine(sum);
