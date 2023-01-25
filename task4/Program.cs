b8f661ae85d540d5d837bfa277357362bc1d415c
Aleksey <lexandreeev@gmail.com>
2023-01-25 21:33:00 +0300

    add files to task3 and add task4

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Показать кубы чисел, заканчивающихся на четную цифру");
+System.Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+double numberCubed = Math.Pow(number, 3);
+if (numberCubed % 2 == 0){
+  System.Console.WriteLine(numberCubed);
+}
+else{
+  System.Console.WriteLine("Число заканчивается на нечетную цифру");
+}
