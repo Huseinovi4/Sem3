997c4d0752497e41e86bdfdaf69a441d14c9d529
Aleksey <lexandreeev@gmail.com>
2023-01-25 21:06:53 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Написать программу вычисления произведения чисел от 1 до N");
+int number = int.Parse(Console.ReadLine()!);
+int multiply = 1;
+for(int i = 1;i<=number;i++){
+multiply = multiply*i;
+}
+System.Console.WriteLine(multiply);
+
